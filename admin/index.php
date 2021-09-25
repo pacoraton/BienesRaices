2d9be4d864c74b74b80adf347c243469eef7dfea
Francisco Sandoval <pacocasino1@hotmail.com>
2021-09-25 10:56:04 -0500

    Se muestran los datos dinamicamente desde la base de datos en una tabla

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,25 @@
 <?php 
+    //Importar conexion
+    require '../build/includes/config/db.php';
+    $db=conectarDB();
+
+    //Escribir la consulta
+
+    $sql="Select * from propiedades";
+
+    //Consultar a la base de datos
+
+    $resultado_consulta=mysqli_query($db,$sql);
+
+
+    //Muestra mensaje consicional
+    $resultado=$_GET['resultado'] ?? null;
+
+    //incluye un tamplate
     require '../build/includes/funciones.php';
     incluirTemplate('header');
 
-    $resultado=$_GET['resultado'] ?? null;
+   
 ?>
 
 <main class="contenedor seccion">
@@ -13,6 +30,41 @@
        <?php }
         ?>
     <a href="/admin/propiedades/crear.php" class=" boton boton-verde">Nueva Propiedad</a>
+
+ 
+  <table class="propiedades">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Titulo</th>
+            <th>Imagen</th>
+            <th>Precio</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    
+    <tbody><!--Mostrar los resultados -->
+          <?php 
+
+            while($propiedades=mysqli_fetch_assoc($resultado_consulta)){ ?>
+          
+            <tr>
+                <td><?php echo $propiedades['id'] ?></td>
+                <td><?php echo $propiedades['titulo']?></td>
+                <td><img src="/imagenes/<?php echo $propiedades['imagenes']; ?>" class="imagen-tabla"></td>
+                <td>$<?php echo  $propiedades['precio']; ?></td>
+                <td>
+                    <a href="#" class="boton boton-rojo">Eliminar</a>
+                    <a href="#" class="boton boton-amarillo">Actualizar</a>
+                </td>
+            </tr>
+
+          <?php 
+            }
+         ?>
+    </tbody>
+  </table>
+
 </main>
 
 <?php
